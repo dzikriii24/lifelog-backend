363e4df108d9595b0e3f509db0d42e459cd97140
dzikriii24 <dzikrirabbani2401@gmail.com>
2025-12-27 21:18:08 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const authMiddleware = require('./middlewares/auth.middleware');
 
 // Middleware CORS
 app.use(cors({
-  origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
+  origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'https://lifelog-two.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
